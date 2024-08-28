2110dc90e57a799c3517767aa69398e0b5360b4b
jeaye <contact@jeaye.com>
2024-08-27 21:18:58 -0700

    Remove largefile plugin
    The treesitter disable predicate should do the trick.

--- lua/package-all/util.lua
+++ lua/package-all/util.lua
@@ -4,12 +4,4 @@ return {
 
   -- Makes some plugins dot-repeatable like leap.
   { "tpope/vim-repeat", event = "VeryLazy" },
-
-  -- Speeds vim up when editing huge file.
-  {
-    "vim-scripts/LargeFile",
-    init = function()
-      vim.g.LargeFile = 0.3
-    end,
-  },
 }
